1703a8e4c13c8272d4de99a9e954def14a634189
StrowBeary <remicaillot5@gmail.com>
2019-04-11 11:13:18 +0200

    encapsulated debugger

--- src/index.js
+++ src/index.js
@@ -1,51 +1,44 @@
-import peter_sound_url from "./assets/sounds/music.wav";
-import {Sound} from "./lib/SoundKit";
+import peter_sound_url from "./assets/sounds/peter pan.wav";
+import music_sound_url from "./assets/sounds/music.wav";
+import {Scene, Sound, SoundKit_debugger} from "./lib/SoundKit";
 
 (async function () {
-
+    const scene = Scene();
     const peter_sound = await Sound({
-        url: peter_sound_url,
+        url: music_sound_url,
         spacialized: true,
         oriented: true,
         orientation: [0, 0, 1],
         loop: true,
-        volume: 0.5,
+        volume: 0.8,
     });
-    peter_sound.play();
+    const music_sound = await Sound({
+        url: peter_sound_url,
+        loop: true,
+        volume: 0.3,
+    });
+    scene.add_child(peter_sound);
+    scene.add_child(music_sound);
+    const sk_debugger = SoundKit_debugger(scene);
     document
         .getElementById("play")
-        .addEventListener("click", () => peter_sound.play());
-
-    const canvas = document.getElementById("viewer");
-    const ctx = canvas.getContext("2d");
-    const dpr = window.devicePixelRatio || 1;
-    console.log("SPR", dpr);
-    ctx.scale(dpr, dpr);
-    const rect = canvas.getBoundingClientRect();
-    canvas.width = rect.width * dpr;
-    canvas.height = rect.height * dpr;
-
+        .addEventListener("click", () => {
+            peter_sound.play();
+            music_sound.play();
+            sk_debugger.play();
+        });
+    const freq_peter = 1 / 500;
     (function loop(t) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        const freq_peter = 1 / 500;
         const pos_peter = [
             Math.cos(2 * Math.PI * t * freq_peter),
             0,
             Math.sin(2 * Math.PI * t * freq_peter),
         ];
 
-
-        ctx.beginPath();
-        ctx.fillStyle = "rgb(255,0,0)";
-        ctx.ellipse(150, 150, 5, 5, 0, 0, 2 * Math.PI);
-        ctx.fill();
-        ctx.closePath();
-
         peter_sound.set_position(pos_peter);
-        //peter_sound.set_orientation([-pos_peter[0], 0, -pos_peter[2]]);
-        peter_sound.canvas_helper(ctx);
-
+        peter_sound.set_orientation([Math.cos(t / 2), 0, Math.sin(t / 2)]);
         requestAnimationFrame(loop.bind({}, t + 1));
     })(0);
+
 
 })();
--- src/lib/SoundKit.js
+++ src/lib/SoundKit.js
@@ -15,6 +15,69 @@ if (!document.hasFocus()) {
     audio_context.suspend();
 }
 
+export function Scene(options) {
+    options = {
+        position: [0, 0, 0],
+        ...options
+    };
+    audio_context.listener.setPosition(...options.position);
+    console.log(audio_context.listener);
+
+    const children = [];
+
+    return {
+        set_position(position) {
+            options.position = position;
+            audio_context.listener.setPosition(...position);
+        },
+        add_child(child) {
+            children.push(child);
+        },
+        _debug: {
+            canvas_helper(ctx, canvas) {
+                const coords = {
+                    x: canvas.width / 2 + options.position[0] * (canvas.width / (2 * window.devicePixelRatio) - 5),
+                    y: canvas.height / 2 + options.position[2] * (canvas.height / (2 * window.devicePixelRatio) - 5)
+                };
+                ctx.beginPath();
+                ctx.fillStyle = "rgb(255,0,0)";
+                ctx.ellipse(coords.x, coords.y, 10, 10, 0, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.closePath();
+            }
+        },
+        children
+    }
+}
+
+export function SoundKit_debugger(scene, options) {
+    const canvas = document.createElement("canvas");
+    canvas.width = 300;
+    canvas.height = 300;
+
+    document.body.appendChild(canvas);
+    let raf = null;
+
+
+    return {
+        play() {
+
+            const ctx = canvas.getContext("2d");
+            (function loop(t) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                scene._debug.canvas_helper(ctx, canvas);
+                scene.children.forEach(c => c._debug.canvas_helper(ctx, canvas));
+
+                raf = requestAnimationFrame(loop.bind({}, t + 1));
+            })(0);
+        },
+        destroy() {
+            cancelAnimationFrame(raf);
+        }
+    }
+}
+
 export async function Sound(options) {
     const default_options = {
         volume: 1,
@@ -25,7 +88,6 @@ export async function Sound(options) {
         oriented: false,
         ...options
     };
-    let next_time = 0;
 
     const gain_node = audio_context.createGain();
     const panner = audio_context.createPanner();
@@ -50,18 +112,20 @@ export async function Sound(options) {
     gain_node.gain.setValueAtTime(default_options.volume, audio_context.currentTime);
 
     const loaded_chunks = [];
-    function schedule_chunk(t) {
-        const chunk = loaded_chunks.shift();
-        const source = chunk.source;
-        source.connect(gain_node);
-        source.start(t);
-        if(loaded_chunks.length > 0) {
-            schedule_chunk(t + source.buffer.duration);
+
+    function schedule_chunk(t, chunk_iterator) {
+        const {value, done} = chunk_iterator.next();
+        if (!done) {
+            const source = value[1].source;
+            source.connect(gain_node);
+            source.start(t);
+            schedule_chunk(t + source.buffer.duration, chunk_iterator);
         }
     }
+
     const response = await fetch(options.url);
     const stream = await response.body.getReader();
-    read_raw_stream(stream,  function (buffer) {
+    read_raw_stream(stream, function (buffer) {
         audio_context.decodeAudioData(buffer).then((audio_buffer) => {
             const source = audio_context.createBufferSource();
             source.buffer = audio_buffer;
@@ -70,16 +134,21 @@ export async function Sound(options) {
                 source,
                 order: buffer.order
             });
-            loaded_chunks.sort((a, b) => a.order-b.order);
+            loaded_chunks.sort((a, b) => a.order - b.order);
         });
     });
 
 
-
-
     return {
-        async play() {
-            schedule_chunk(audio_context.currentTime + 0.01);
+        play() {
+            console.log(options.url, loaded_chunks);
+            schedule_chunk(audio_context.currentTime + 0.01, loaded_chunks.entries());
+        },
+        pause() {
+            console.log(gain_node);
+        },
+        stop() {
+
         },
         set_position(position) {
             if (options.spacialized) {
@@ -94,40 +163,56 @@ export async function Sound(options) {
         set_volume(volume) {
             gain_node.gain.setValueAtTime(volume, audio_context.currentTime)
         },
-        canvas_helper(ctx) {
-            const coords = {
-                x: 150 + panner.positionX.value * 145,
-                y: 150 + panner.positionZ.value * 145
-            };
+        _debug: {
+            canvas_helper(ctx, canvas) {
+                const coords = {
+                    x: canvas.width / 2 + panner.positionX.value * (canvas.width / (2 * window.devicePixelRatio) - 5),
+                    y: canvas.height / 2 + panner.positionZ.value * (canvas.height / (2 * window.devicePixelRatio) - 5)
+                };
 
-            ctx.strokeStyle = "rgb(0, 0, 0)";
-            ctx.lineWidth = 2;
-            ctx.fillStyle = "rgb(0,0,0)";
+                ctx.strokeStyle = "rgb(0, 0, 0)";
+                ctx.lineWidth = 2;
+                ctx.fillStyle = "rgb(0,0,0)";
 
-            ctx.beginPath();
-            const angle = (panner.coneInnerAngle / 2) / 360 * 2 * Math.PI;
-            const oriented_angle = -angle_vector([1, 0, 0], [panner.orientationX.value, 0, panner.orientationZ.value]);
-            ctx.ellipse(coords.x, coords.y, 20, 20, oriented_angle, -angle, angle);
-            ctx.stroke();
-            ctx.closePath();
-            if (options.spacialized) {
-                if (options.oriented) {
+                ctx.beginPath();
+                const angle = (panner.coneInnerAngle / 2) / 360 * 2 * Math.PI;
+                const oriented_angle = -angle_vector([1, 0, 0], [panner.orientationX.value, 0, panner.orientationZ.value]);
+                ctx.ellipse(coords.x, coords.y, 20, 20, oriented_angle, -angle, angle);
+                ctx.stroke();
+                ctx.closePath();
+                if (options.spacialized) {
+                    if (options.oriented) {
+                        ctx.beginPath();
+                        canvas_arrow(ctx, coords.x, coords.y, panner.orientationX.value * 75, panner.orientationZ.value * 75);
+                        ctx.stroke();
+                        ctx.closePath();
+                    }
                     ctx.beginPath();
-                    canvas_arrow(ctx, coords.x, coords.y, panner.orientationX.value * 75, panner.orientationZ.value * 75);
-                    ctx.stroke();
+                    ctx.ellipse(coords.x, coords.y, 5, 5, 0, 0, 2 * Math.PI);
+                    ctx.fill();
+                    ctx.closePath();
+                    ctx.beginPath();
+                    ctx.font = "14px Arial";
+                    ctx.textAlign = Math.sign(panner.positionX.value) === 1 ? "left" : "right";
+                    ctx.fillText(options.url, coords.x + Math.sign(panner.positionX.value) * 20, coords.y + 5);
+                    ctx.fill();
+                    ctx.closePath();
+                } else {
+                    ctx.beginPath();
+                    ctx.font = "14px Arial";
+                    ctx.textAlign = "left";
+                    ctx.fillText(options.url, canvas.width / 2 + 30, canvas.height / 2 + 5);
+                    ctx.fill();
+                    ctx.closePath();
+
+                    ctx.beginPath();
+                    ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+                    ctx.ellipse(coords.x, coords.y, 15, 15, 0, 0, 2 * Math.PI);
+                    ctx.fill();
                     ctx.closePath();
                 }
-                ctx.beginPath();
-                ctx.ellipse(coords.x, coords.y, 5, 5, 0, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.closePath();
-            } else {
-                ctx.beginPath();
-                ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
-                ctx.ellipse(coords.x, coords.y, 15, 15, 0, 0, 2 * Math.PI);
-                ctx.fill();
-                ctx.closePath();
-            }
+
+            },
 
         }
     }
