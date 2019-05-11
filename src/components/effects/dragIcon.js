f4ddd0431c0295511ac9859fda4b84c13743cd98
mel <melanie.ngo@edu.gobelins.fr>
2019-05-11 17:17:41 +0200

    Finish creating dragging icon

--- src/components/effects/dragIcon.js
+++ src/components/effects/dragIcon.js
@@ -2,10 +2,11 @@ import * as PIXI from "pixi.js";
 import {Animate, Easing} from "lib/TimingKit";
 
 let Graphics = PIXI.Graphics;
+const lineWidth = 100;
+let newPos;
 
 export function DragIcon() {
     this.alpha = 0;
-    this.direction = 1;
 }
 
 DragIcon.prototype.initIconAnim = function(from_value, to_value) {
@@ -29,45 +30,55 @@ DragIcon.prototype.startSlideAnim = function() {
 };
 
 DragIcon.prototype.setPosition = function(x, y) {
-    this.directionLine.position.set(x, y);
     this.interactiveIcon.position.set(x, y);
-    this.startPosition = x;
-    this.finalPosition = this.direction * 100;
 };
 
 DragIcon.prototype.setDirection = function(direction) {
-    this.direction = direction;
-    this.directionLine.clear();
-    this.directionLine.lineStyle(2, 0xFFFFFF, 1);
-    if (direction === -1) {
-        this.directionLine.moveTo(-15, 0);
-        this.directionLine.lineTo(-100, 0);
-    } else if (direction === 1) {
-        this.directionLine.moveTo(15, 0);
-        this.directionLine.lineTo(100, 0);
-    }
-    this.directionLine.alpha = this.alpha;
+    this.finalPosition = direction * lineWidth;
 };
 
 DragIcon.prototype.setup = function(app) {
     this.interactiveIcon = new Graphics();
-    this.directionLine = new Graphics();
 
     this.interactiveIcon.lineStyle(2, 0xFFFFFF, 1);
     this.interactiveIcon.drawCircle(0, 0, 15);
     this.interactiveIcon.alpha = this.alpha;
 
-    app.stage.addChild(this.directionLine);
     app.stage.addChild(this.interactiveIcon);
+};
+
+DragIcon.prototype.draw = function(newPos) {
+    this.interactiveIcon.clear();
+    this.interactiveIcon.lineStyle(2, 0xFFFFFF, 1);
+    this.interactiveIcon.drawCircle(newPos, 0, 15);
+    if (this.finalPosition > 0) {
+        if (newPos > 15) {
+            this.interactiveIcon.moveTo(0, 0);
+            this.interactiveIcon.lineTo(newPos - 15, 0);
+        }
+        if (newPos + 15 < this.finalPosition) {
+            this.interactiveIcon.moveTo(newPos + 15, 0);
+            this.interactiveIcon.lineTo(this.finalPosition, 0);
+        }
+    } else if (this.finalPosition < 0) {
+        if (newPos < -15) {
+            this.interactiveIcon.moveTo(0, 0);
+            this.interactiveIcon.lineTo(newPos + 15, 0);
+        }
+        if (newPos - 15 > this.finalPosition) {
+            this.interactiveIcon.moveTo(newPos - 15, 0);
+            this.interactiveIcon.lineTo(this.finalPosition, 0);
+        }
+    }
 };
 
 DragIcon.prototype.loop = function() {
     if (this.iconAnim && this.iconAnim.is_running) {
         this.interactiveIcon.alpha = this.iconAnim.tick();
-        this.directionLine.alpha = this.iconAnim.tick();
     }
     if (this.slideAnim && this.slideAnim.is_running) {
-        this.interactiveIcon.position.set(this.finalPosition * this.slideAnim.tick() + this.startPosition, this.interactiveIcon.position.y);
+        newPos = this.finalPosition * this.slideAnim.tick();
+        this.draw(newPos);
     } else {
         this.initSlideAnim(0, 1);
         this.startSlideAnim();
