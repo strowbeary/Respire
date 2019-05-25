0b2e88da39941f58db1b73f481ca9e17ca200412
mel <melanie.ngo@edu.gobelins.fr>
2019-05-25 10:32:40 +0200

    Création de close eye

--- src/components/experiments/OpenEye.svelte
+++ src/components/experiments/OpenEye.svelte
@@ -29,7 +29,6 @@
         app = data.detail.app;
         canvasWidth = data.detail.canvasWidth;
         canvasHeight = data.detail.canvasHeight;
-        app.stage.addChild(graphics);
 
         if (!resources[backgroundImg]) {
             loader
@@ -43,26 +42,50 @@
 
     function setup() {
         background = new Sprite(resources[backgroundImg].texture);
-        app.stage.addChild(background);
         background.scale.set(canvasWidth/background.width);
+        app.stage.addChild(background);
+        app.stage.addChild(graphics);
 
+        initCloseEye();
+
+        app.ticker.add(delta => gameLoop(delta));
+    }
+
+    function initOpenEye() {
         graphics.beginFill(0x000000);
         graphics.moveTo(0, 0);
         graphics.bezierCurveTo(50, -100, canvasWidth - 50, -100, canvasWidth, 0);
         graphics.moveTo(0, 0);
         graphics.bezierCurveTo(50, 100, canvasWidth - 50, 100, canvasWidth, 0);
-        graphics.position.set(0, canvasHeight/2);
+        graphics.position.set(canvasWidth/2, canvasHeight/2);
         graphics.endFill();
-
-        app.stage.addChild(graphics);
-
         background.mask = graphics;
         background.filters = [new filters.BlurFilter()];
 
-        app.ticker.add(delta => gameLoop(delta));
         scaleAnim = Animate(1, 8.5, Easing.bounceOut, 0.005);
         heightAnim = Animate(0, 2, Easing.bounceOut, 0.005);
-        blurAnim = Animate(16, 0, Easing.bounceOut, 0.01);
+        blurAnim = Animate(4, 0, Easing.bounceOut, 0.005);
+        scaleAnim.start();
+        heightAnim.start();
+        blurAnim.start();
+    }
+
+    function initCloseEye() {
+        graphics.beginFill(0x000000);
+        graphics.moveTo(0, 0);
+        graphics.bezierCurveTo(50, -100 * 2, canvasWidth - 50, -100 * 2, canvasWidth, 0);
+        graphics.moveTo(0, 0);
+        graphics.bezierCurveTo(50, 100 * 2, canvasWidth - 50, 100 * 2, canvasWidth, 0);
+        graphics.position.set(canvasWidth/2, canvasHeight/2);
+        graphics.pivot.set(canvasWidth/2, height/2);
+        graphics.scale.set(4);
+        graphics.endFill();
+        background.mask = graphics;
+        background.filters = [new filters.BlurFilter(4, 4)];
+
+        scaleAnim = Animate(6, 1, Easing.bounceOut, 0.0025);
+        heightAnim = Animate(1.5, 0, Easing.bounceOut, 0.0025);
+        blurAnim = Animate(0, 4, Easing.bounceOut, 0.0025);
         scaleAnim.start();
         heightAnim.start();
         blurAnim.start();
@@ -73,7 +96,7 @@
         if (scaleAnim.is_running || heightAnim.is_running) {
             height = heightAnim.tick();
             scale = scaleAnim.tick();
-
+            console.log(height, scale);
             graphics.clear();
             graphics.beginFill(0x000000);
             graphics.moveTo(0, 0);
@@ -90,7 +113,7 @@
             background.filters = [new filters.BlurFilter(blurAnim.tick(), 4)];
         }
         if (blurAnim.is_ended_signal) {
-            background.filters = [];
+            //background.filters = [];
         }
     }
 </script>
