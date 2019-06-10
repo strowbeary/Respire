53ab98852fdad7f352787bd3a0c6feffa2dd7b9e
StrowBeary <remicaillot5@gmail.com>
2019-06-10 16:43:31 +0200

    clock sound

--- src/components/experiments/Pilule/Pillule.sound.js
+++ src/components/experiments/Pilule/Pillule.sound.js
@@ -2,6 +2,7 @@ import { Scene, Sound } from "lib/SoundKit";
 import loop_sound_url from "assets/sounds/Pillules/Boucle.flac";
 import pop_sound_url from "assets/sounds/Pillules/pop.flac";
 import writing_sound_url from "assets/sounds/Pillules/writing.flac";
+import clock_sound_url from "assets/sounds/Pillules/clock tick.wav";
 import {Animate, Easing} from "lib/TimingKit";
 
 
@@ -26,13 +27,20 @@ export async function init_pillule_sound_scene() {
             volume: 1,
             loop: true,
         }),
+        Sound("clock", {
+            url: clock_sound_url,
+            volume: 1,
+            loop: true,
+        }),
     );
 
     const init_scene = await scene.init();
 
     const writing_sound = init_scene.get_children_by_name("writing");
+    const clock_sound = init_scene.get_children_by_name("clock");
     const pill_shake_sound = init_scene.get_children_by_name("pill_shake");
     writing_sound.play();
+    clock_sound.play();
 
     let writing_animation = Animate(0, 1, Easing.linear, 0.006);
     let pill_shake_animation = Animate(0, 1, Easing.linear, 0.03);
@@ -40,6 +48,7 @@ export async function init_pillule_sound_scene() {
     (function loop(t) {
         if(writing_animation.is_running) {
             writing_sound.set_volume(writing_animation.tick());
+            clock_sound.set_volume(writing_animation.tick());
         }
         if(pill_shake_animation.is_running) {
             pill_shake_sound.set_volume(pill_shake_animation.tick());
@@ -57,8 +66,8 @@ export async function init_pillule_sound_scene() {
             cancelAnimationFrame(req_id);
             init_scene.destroy();
         },
-        fade_out_writing(duration = 0.002) {
-            writing_animation = Animate(1, 0, Easing.linear, duration);
+        fade_out_writing() {
+            writing_animation = Animate(1, 0, Easing.linear, 0.001);
             writing_animation.start();
         },
         fade_in_writing() {
