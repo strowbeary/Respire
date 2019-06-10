f8a9e4c5e59b0712adb4b2d4c62819e729ab414c
StrowBeary <remicaillot5@gmail.com>
2019-06-10 18:08:15 +0200

    low pass on writing

--- src/components/experiments/Pilule/Pillule.sound.js
+++ src/components/experiments/Pilule/Pillule.sound.js
@@ -5,6 +5,7 @@ import pop_sound_url from "assets/sounds/Pillules/pop.flac";
 import writing_sound_url from "assets/sounds/Pillules/writing.flac";
 import clock_sound_url from "assets/sounds/Pillules/clock tick.wav";
 import {Animate, Easing} from "lib/TimingKit";
+import {LowPassEffect} from "lib/SoundKit/effects/LowPassEffect";
 
 
 export async function init_pillule_sound_scene() {
@@ -49,13 +50,20 @@ export async function init_pillule_sound_scene() {
     writing_sound.play();
     clock_sound.play();
 
+
+    const low_pass_filter = writing_sound.add_effect(LowPassEffect);
+
     let clock_animation = Animate(0, 1, Easing.linear, 0.006);
     let writing_animation = Animate(0, 1, Easing.linear, 0.006);
+    let writing_lp_animation = Animate(1, 1, Easing.linear, 0);
     let pill_shake_animation = Animate(0, 1, Easing.linear, 0.03);
     let req_id = null;
     (function loop(t) {
         if(writing_animation.is_running) {
             writing_sound.set_volume(writing_animation.tick());
+        }
+        if(writing_lp_animation.is_running) {
+            low_pass_filter.set_frequency(writing_lp_animation.tick() * 20000);
         }
         if(clock_animation.is_running) {
             clock_sound.set_volume(clock_animation.tick());
@@ -78,12 +86,12 @@ export async function init_pillule_sound_scene() {
             init_scene.destroy();
         },
         fade_out_writing() {
-            writing_animation = Animate(1, 0, Easing.linear, 0.002);
-            writing_animation.start();
+            writing_lp_animation = Animate(1, 0, Easing.linear, 0.002);
+            writing_lp_animation.start();
         },
         fade_in_writing() {
-            writing_animation = Animate(0, 1, Easing.linear, 0.002);
-            writing_animation.start();
+            writing_lp_animation = Animate(0, 1, Easing.linear, 0.002);
+            writing_lp_animation.start();
         },
         start_pills_shake() {
             pill_shake_sound.play();
